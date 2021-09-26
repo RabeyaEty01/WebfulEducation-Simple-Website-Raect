46ec22f0052a4745f38096fe3caa94b09bd724fc
RabeyaEty01 <etyrabeya01@gmail.com>
2021-09-26 13:20:38 +0600

    all icons added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import './Cart.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDollarSign, faBook, faShoppingCart}from '@fortawesome/free-solid-svg-icons';
 
 const Cart = (props) => {
     const { cart } = props;
+    //icon
+    const dollerIcon = <FontAwesomeIcon className="text-success" icon={faDollarSign} />
+    const bookIcon = <FontAwesomeIcon className="text-success" icon={faBook} />
+    const cartIcon = <FontAwesomeIcon className="text-danger" icon={faShoppingCart} />
 
     const totalReducer = (previous, course) => previous + course.price;
     const total = cart.reduce(totalReducer, 0);
 
     return (
         <div className="sticky-top">
-             <h4 className="text-center fw-bold pb-3">Order Summery</h4>
+             <h4 className="text-center fw-bold pb-3">Order Summery{cartIcon}</h4>
             <table class="table table-bordered border-primary">
                 <thead>
                     <tr className="table-active">
-                        <th scope="col">Course Added : </th>
+                        <th scope="col">{bookIcon} Course Added :</th>
                         <th scope="col"> {props.cart.length}</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr className="table-active">
-                        <th scope="row">Total Cost :</th>
+                        <th scope="row">{dollerIcon} Total Cost :</th>
                         <td className="fw-bold"> ${total.toFixed(2)}</td>
                     </tr>
                 </tbody>
--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faClock,faCalendarAlt,faChalkboardTeacher,faBars ,faDollarSign}from '@fortawesome/free-solid-svg-icons';
 
 const Courses = (props) => {
     const { name, img, startDate, duration, instructors, credit, price, } = props.course;
+    //All icons
+    const arrowIcon = <FontAwesomeIcon icon={faArrowRight} />
+    const clockIcon = <FontAwesomeIcon className="text-success" icon={faClock} />
+    const calenderIcon = <FontAwesomeIcon className="text-success" icon={faCalendarAlt} />
+    const teacherIcon = <FontAwesomeIcon className="text-success" icon={faChalkboardTeacher} />
+    const barIcon = <FontAwesomeIcon className="text-success" icon={faBars} />
+    const dollerIcon = <FontAwesomeIcon className="text-success" icon={faDollarSign} />
 
     return (
         <div>
@@ -9,13 +18,13 @@ const Courses = (props) => {
                 <img className="img-fluid p-2" src={img} alt="" />
                 <div class="card-body">
                     <h5 class="card-title text-primary fw-bold">{name}</h5>
-                    <p class="card-text text-start"><small>Instructors: {instructors}</small></p>
-                    <p class="card-text text-start"><small>Start Date: {startDate}</small></p>
-                    <p class="card-text text-start"><small>Duration: {duration}</small></p>
-                    <p class="card-text text-start"><small>Credit: {credit}</small></p>
+                    <p class="card-text text-start"><small>{teacherIcon} Instructors: {instructors}</small></p>
+                    <p class="card-text text-start"><small>{clockIcon} Start Date:  {startDate}</small></p>
+                    <p class="card-text text-start"><small>{calenderIcon} Duration: {duration}</small></p>
+                    <p class="card-text text-start"><small>{barIcon} Credit: {credit}</small></p>
                     <div class="d-flex justify-content-between">
-                        <h5 class="card-title fw-bold">Price: ${price}</h5>
-                        <button class="rounded-3 bg-warning border-1 fw-bold"  onClick={() => props.handleAddToCart(props.course)} >Enroll Now</button>
+                        <h5 class="card-title fw-bold">Price: {dollerIcon}{price}</h5>
+                        <button class="rounded-3 bg-warning border-1 fw-bold"  onClick={() => props.handleAddToCart(props.course)} >Enroll Now {arrowIcon}</button>
                     </div>
 
                 </div>
