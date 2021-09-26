e141e30d0d1a93f2bef5a8ffa0385d1ddd2e15c7
RabeyaEty01 <etyrabeya01@gmail.com>
2021-09-26 16:28:37 +0600

    name card style added

--- src/components/NameCard/NameCard.js
+++ src/components/NameCard/NameCard.js
@@ -2,14 +2,19 @@ import './NameCard.css';
 
 const NameCard = (props) => {
     //console.log(props.nameCard);
-    const { name } = props.nameCard;
-
+    const { name, img } = props.nameCard;
 
     return (
-
-        <div class="card">
-            <div class="card-body">
-                <p>{name} </p>
+        <div class="card mb-3 bg-light bg-gradient">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img className="img-fluid p-1" src={img} alt="" />
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <p class="card-title fw-bold text-primary">{name}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
